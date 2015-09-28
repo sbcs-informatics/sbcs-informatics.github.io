6968dd7a32ca64ce1879413ed13b388a0b9fdc00
IsmailM <ismail.moghul@gmail.com>
2015-09-28 10:31:24 +0000

    build website at bb9a461

---
+++ deploy.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+set -o errexit -o nounset
+
+rev=$(git rev-parse --short HEAD)
+
+cd _book
+git init
+
+git config user.name "IsmailM"
+git config user.email "ismail.moghul@gmail.com"
+git remote add deploy "https://$GH_TOKEN@github.com/sbcs-informatics/sbcs-informatics.github.io.git"
+
+git fetch deploy master
+git reset deploy/master
+
+git add -A .
+git commit -m "build website at ${rev}"
+git push deploy HEAD:master
